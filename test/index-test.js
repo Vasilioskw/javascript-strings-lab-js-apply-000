05f5ab06857bcdf6de53341a2db49ac9b7cf7a82
adaptable-illustrator-5739 <vasilioskw@gmail.com>
2020-08-08 16:39:25 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,11 +1,11 @@
 /*global concatenatedString, describe, interpolatedString, it, myString */
 
-it('defines `greeting`', function() {
+it('defines `Hello, everyone!`', function() {
   expect(greeting).toEqual("Hello, everybody!")
 })
 
-it('concatenates strings to greet a special guest in `greetSpecialGuest`', function() {
-  expect(greetSpecialGuest).toEqual(`Hello, ${specialGuest}!`)
+it('concatenates strings to greet a special guest in SpecialGuest`', function() {
+  expect(SpecialGuest).toEqual(`Hello, ${specialGuest}!`)
 })
 
 it('interpolates a string in `conversation`', function() {
