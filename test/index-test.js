97fd885973f27055507daf4a692b88d4e3f2e0ed
adaptable-illustrator-5739 <vasilioskw@gmail.com>
2020-08-08 16:57:18 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -11,11 +11,11 @@ it('concatenates strings to greet a special guest in SpecialGuest`', function() 
 it('interpolates a string in `conversation`', function() {
   expect(conversation).toEqual(`Let's talk about ${topic}.`)
 })
-var greeting = "Hello, everyone!";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "Hello, everyone!" + specialGuest + "!";
+var greetSpecialGuest = "Hello, everybody!" + specialGuest + "!";
 
 var topic = "space";
 
